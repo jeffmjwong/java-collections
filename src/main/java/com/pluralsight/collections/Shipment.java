180b25e9b81fc86972a85d8cbb1bb623a7d2925e
Jeff Wong <v-jwong1@expedia.com>
2020-01-03 15:12:35 +1000

    complete Shipment class details

--- src/main/java/com/pluralsight/collections/Shipment.java
+++ src/main/java/com/pluralsight/collections/Shipment.java
@@ -1,4 +1,26 @@
 package com.pluralsight.collections;
 
-public class Shipment {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Shipment implements Iterable<Product> {
+    private static final int LIGHT_VAN_MAX_WEIGHT = 20;
+    private List<Product> products = new ArrayList<>();
+
+    public void add(Product product) {
+        this.products.add(product);
+    }
+
+    public void replace(Product oldProduct, Product newProduct) {
+        final int index = this.products.indexOf(oldProduct);
+
+        if (index != -1) {
+            this.products.set(index, newProduct);
+        }
+    }
+
+    public Iterator<Product> iterator() {
+        return this.products.iterator();
+    }
 }
