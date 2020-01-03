b65eac1191d724e4ae87a2d7997eb69eed9644ee
Jeff Wong <v-jwong1@expedia.com>
2020-01-03 15:02:53 +1000

    create Product class

--- src/test/java/com/pluralsight/collections/ShipmentTest.java
+++ src/test/java/com/pluralsight/collections/ShipmentTest.java
@@ -1,4 +1,17 @@
 package com.pluralsight.collections;
 
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
 public class ShipmentTest {
+    private Shipment shipment = new Shipment();
+
+    @Test
+    public void shouldAddItems() throws Exception {
+        this.shipment.add(door);
+        this.shipment.add(window);
+
+        assertThat(shipment, contains(door, window));
+    }
 }
