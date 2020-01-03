3fc845cbe4caaa6c8c32a80fcce591afe863ce6c
Jeff Wong <v-jwong1@expedia.com>
2020-01-03 15:18:41 +1000

    complete ShipmentTest and ProductFixtures

--- src/test/java/com/pluralsight/collections/ShipmentTest.java
+++ src/test/java/com/pluralsight/collections/ShipmentTest.java
@@ -1,6 +1,9 @@
 package com.pluralsight.collections;
 
 import org.junit.Test;
+
+import static com.pluralsight.collections.ProductFixtures.door;
+import static com.pluralsight.collections.ProductFixtures.window;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
@@ -9,8 +12,8 @@ public class ShipmentTest {
 
     @Test
     public void shouldAddItems() throws Exception {
-        this.shipment.add(door);
-        this.shipment.add(window);
+        shipment.add(door);
+        shipment.add(window);
 
         assertThat(shipment, contains(door, window));
     }
