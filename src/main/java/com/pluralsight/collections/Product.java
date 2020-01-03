c1d2da39ca4d0f6b1f2fa42aeb1fd70796632faa
Jeff Wong <v-jwong1@expedia.com>
2020-01-03 15:05:08 +1000

    create Shipment class

--- src/main/java/com/pluralsight/collections/Product.java
+++ src/main/java/com/pluralsight/collections/Product.java
@@ -1,4 +1,23 @@
 package com.pluralsight.collections;
 
 public class Product {
+    private final String name;
+    private final int weight;
+
+    public String getName() {
+        return this.name;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name='" + this.name + "'" + ", weight=" + this.weight + "}";
+    }
 }
