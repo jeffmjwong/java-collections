025432cc2c739898eca4dac20de8ce1707623670
Jeff Wong <v-jwong1@expedia.com>
2020-01-04 16:45:30 +1000

    use an example of HashSet

--- src/main/java/com/pluralsight/collections/Main.java
+++ src/main/java/com/pluralsight/collections/Main.java
@@ -1,7 +1,24 @@
 package com.pluralsight.collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java collections!");
+
+        final Set<Product> products = new HashSet<>();
+        System.out.println(products);
+        final Product glassDoor = new Product("Glass Door", 50);
+        products.add(glassDoor);
+        products.add(glassDoor);
+        products.add(glassDoor);
+        products.add(glassDoor);
+        products.add(glassDoor);
+        products.add(glassDoor);
+
+        System.out.println(products);
     }
 }
