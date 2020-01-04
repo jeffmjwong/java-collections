c0f46cf618a961e7b9bca26d835f22c2abd4303c
Jeff Wong <v-jwong1@expedia.com>
2020-01-04 20:23:13 +1000

    use a parameterized interface instead of raw parameter

--- src/main/java/com/pluralsight/collections/ProductCatalogue.java
+++ src/main/java/com/pluralsight/collections/ProductCatalogue.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class ProductCatalogue implements Iterable {
+public class ProductCatalogue implements Iterable<Product> {
     private final Set<Product> products = new TreeSet<>(Product.BY_NAME);
 
     public Set<Product> getProducts() {
