e65dd538cf6a46e2a5c978ace3ba316d98b5dbcd
Jeff Wong <v-jwong1@expedia.com>
2020-01-06 19:41:41 +1000

    complete enquire and processAllEnquiries methods in HelpDesk class

--- src/main/java/com/pluralsight/collections/HelpDesk.java
+++ src/main/java/com/pluralsight/collections/HelpDesk.java
@@ -1,15 +1,17 @@
 package com.pluralsight.collections;
 
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class HelpDesk {
-    private Queue<Enquiry> enquiries;
+    private Queue<Enquiry> enquiries = new ArrayDeque<>();
 
     public void enquire(final Customer customer, final Category category) {
-
+        this.enquiries.offer(new Enquiry(customer, category));
     }
 
     public void processAllEnquiries() {
-
+        final Enquiry enquiry = this.enquiries.remove();
+        enquiry.getCustomer().reply("Have you tried turning it off and on again?");
     }
 }
