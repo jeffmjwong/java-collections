628beedca97c62573dfb479a9430b45d95db42a1
Jeff Wong <v-jwong1@expedia.com>
2020-01-06 19:37:22 +1000

    finish implementing Enquiry class

--- src/main/java/com/pluralsight/collections/Enquiry.java
+++ src/main/java/com/pluralsight/collections/Enquiry.java
@@ -1,4 +1,23 @@
 package com.pluralsight.collections;
 
 public class Enquiry {
+    private final Customer customer;
+    private final Category category;
+
+    public Customer getCustomer() {
+        return this.customer;
+    };
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public Enquiry(Customer customer, Category category) {
+        this.customer = customer;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Enquiry{" + "customer='" + this.customer + "'" + ", category=" + this.category + "}";
+    }
 }
