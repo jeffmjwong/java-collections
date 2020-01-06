efc410dcc6854f8b167485711c0036f927daed4a
Jeff Wong <v-jwong1@expedia.com>
2020-01-06 19:34:55 +1000

    create Enquiry class

--- src/main/java/com/pluralsight/collections/HelpDesk.java
+++ src/main/java/com/pluralsight/collections/HelpDesk.java
@@ -1,6 +1,10 @@
 package com.pluralsight.collections;
 
+import java.util.Queue;
+
 public class HelpDesk {
+    private Queue<Enquiry> enquiries;
+
     public void enquire(final Customer customer, final Category category) {
 
     }
