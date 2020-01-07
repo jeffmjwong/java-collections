0de6cc1f8b676589454757d9e23c46c5ef08ce80
Jeff Wong <v-jwong1@expedia.com>
2020-01-07 18:11:41 +1000

    implements Comparator in PriorityHelpDesk class

--- src/main/java/com/pluralsight/collections/PriorityHelpDesk.java
+++ src/main/java/com/pluralsight/collections/PriorityHelpDesk.java
@@ -1,11 +1,18 @@
 package com.pluralsight.collections;
 
-import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.function.Predicate;
 
 public class PriorityHelpDesk {
-    private Queue<Enquiry> enquiries = new ArrayDeque<>();
+    private static final Comparator<Enquiry> BY_CATEGORY = new Comparator<Enquiry>() {
+        @Override
+        public int compare(final Enquiry o1, final Enquiry o2) {
+            return o1.getCategory().compareTo(o2.getCategory());
+        }
+    };
+
+    private Queue<Enquiry> enquiries = new PriorityQueue<>(BY_CATEGORY);
 
     public void enquire(final Customer customer, final Category category) {
         this.enquiries.offer(new Enquiry(customer, category));
