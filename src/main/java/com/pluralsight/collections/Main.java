c8d8a76fc9ebbdf72058eb9f14cfdf0d0413ea16
Jeff Wong <v-jwong1@expedia.com>
2020-01-07 18:02:34 +1000

    create helpDesk instance of PriorityHelpDesk and process all enquiries

--- src/main/java/com/pluralsight/collections/Main.java
+++ src/main/java/com/pluralsight/collections/Main.java
@@ -7,13 +7,21 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        CategorizedHelpDesk helpDesk = new CategorizedHelpDesk();
+//        CategorizedHelpDesk helpDesk = new CategorizedHelpDesk();
+//
+//        helpDesk.enquire(Customer.JACK, Category.PHONE);
+//        helpDesk.enquire(Customer.JILL, Category.PRINTER);
+//
+//        helpDesk.processPrinterEnquiry();
+//        helpDesk.processGeneralEnquiry();
+//        helpDesk.processPrinterEnquiry();
+
+        PriorityHelpDesk helpDesk = new PriorityHelpDesk();
 
         helpDesk.enquire(Customer.JACK, Category.PHONE);
         helpDesk.enquire(Customer.JILL, Category.PRINTER);
+        helpDesk.enquire(Customer.MARY, Category.COMPUTER);
 
-        helpDesk.processPrinterEnquiry();
-        helpDesk.processGeneralEnquiry();
-        helpDesk.processPrinterEnquiry();
+        helpDesk.processAllEnquiries();
     }
 }
--- src/main/java/com/pluralsight/collections/PriorityHelpDesk.java
+++ src/main/java/com/pluralsight/collections/PriorityHelpDesk.java
@@ -1,4 +1,21 @@
 package com.pluralsight.collections;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.function.Predicate;
+
 public class PriorityHelpDesk {
+    private Queue<Enquiry> enquiries = new ArrayDeque<>();
+
+    public void enquire(final Customer customer, final Category category) {
+        this.enquiries.offer(new Enquiry(customer, category));
+    }
+
+    public void processAllEnquiries() {
+        Enquiry enquiry;
+        while ((enquiry = this.enquiries.poll()) != null) {
+            enquiry.getCustomer().reply("Have you tried turning it off and on again?");
+        }
+    }
 }
+
