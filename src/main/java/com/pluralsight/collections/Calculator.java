7dfee6d1e3dcf318dd6db3151667086c0b46a940
Jeff Wong <v-jwong1@expedia.com>
2020-01-08 18:26:43 +1000

    complete evaluate method in Calculator class

--- src/main/java/com/pluralsight/collections/Calculator.java
+++ src/main/java/com/pluralsight/collections/Calculator.java
@@ -1,7 +1,35 @@
 package com.pluralsight.collections;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import static java.lang.Integer.parseInt;
+
 public class Calculator {
     public int evaluate(final String input) {
-        return Integer.parseInt(input);
+        final Deque<String> stack = new ArrayDeque<>();
+        final String[] tokens = input.split(" ");
+
+        Collections.addAll(stack, tokens);
+
+        while (stack.size() > 1) {
+            final int left = parseInt(stack.pop());
+            final String operator = stack.pop();
+            final int right = parseInt(stack.pop());
+
+            int result = 0;
+            switch(operator) {
+                case "+":
+                    result = left + right;
+                    break;
+                case "-":
+                    result = left - right;
+                    break;
+            }
+
+            stack.push(String.valueOf(result));
+        }
+
+        return parseInt(stack.pop());
     }
 }
--- src/main/java/com/pluralsight/collections/Main.java
+++ src/main/java/com/pluralsight/collections/Main.java
@@ -1,27 +1,17 @@
 package com.pluralsight.collections;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-//        CategorizedHelpDesk helpDesk = new CategorizedHelpDesk();
-//
-//        helpDesk.enquire(Customer.JACK, Category.PHONE);
-//        helpDesk.enquire(Customer.JILL, Category.PRINTER);
-//
-//        helpDesk.processPrinterEnquiry();
-//        helpDesk.processGeneralEnquiry();
-//        helpDesk.processPrinterEnquiry();
+        Calculator calculator = new Calculator();
 
-        PriorityHelpDesk helpDesk = new PriorityHelpDesk();
-
-        helpDesk.enquire(Customer.JACK, Category.PHONE);
-        helpDesk.enquire(Customer.JILL, Category.PRINTER);
-        helpDesk.enquire(Customer.MARY, Category.COMPUTER);
-
-        helpDesk.processAllEnquiries();
+        int r = calculator.evaluate("10 - 3 + 1 + 12");
+        System.out.println(r);
     }
 }
