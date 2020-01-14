60ccea01a0b829c960c97719d08e98fb4f624a7c
Jeff Wong <v-jwong1@expedia.com>
2020-01-14 20:23:26 +1000

    complete MapProductLookupTable class

--- src/main/java/com/pluralsight/collections/MapProductLookupTable.java
+++ src/main/java/com/pluralsight/collections/MapProductLookupTable.java
@@ -1,4 +1,29 @@
 package com.pluralsight.collections;
 
-public class MapProductLookupTable {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProductLookupTable implements ProductLookupTable {
+    private Map<Integer, Product> idToProduct = new HashMap<>();
+
+    @Override
+    public Product lookupById(int id) {
+        return idToProduct.get(id);
+    }
+
+    @Override
+    public void addProduct(Product productToAdd) {
+        final int id = productToAdd.getId();
+
+        if (idToProduct.containsKey(id)) {
+            throw new IllegalArgumentException("Unable to add product, duplicate ID for " + productToAdd);
+        }
+
+        idToProduct.put(id, productToAdd);
+    }
+
+    @Override
+    public void clear() {
+        idToProduct.clear();
+    }
 }
