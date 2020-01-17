54a1e715ecf7eeab9c2f1e8084c95c24bcefbd38
Jeff Wong <v-jwong1@expedia.com>
2020-01-17 20:31:46 +1000

    use sort method on instances of List themselves

--- src/main/java/com/pluralsight/collections/Main.java
+++ src/main/java/com/pluralsight/collections/Main.java
@@ -22,11 +22,11 @@ public class Main {
         System.out.println(products);
         System.out.println();
 
-        Collections.shuffle(products);
+        products.sort(Product.BY_NAME);
         System.out.println(products);
         System.out.println();
 
-        Collections.shuffle(products);
+        products.sort(Product.BY_WEIGHT);
         System.out.println(products);
         System.out.println();
     }
