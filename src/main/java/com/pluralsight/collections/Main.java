30b3b0d87d7b0b575e3546779c3d51e08efcc7ff
Jeff Wong <v-jwong1@expedia.com>
2020-01-17 20:10:57 +1000

    use Collections.rotate method

--- src/main/java/com/pluralsight/collections/Main.java
+++ src/main/java/com/pluralsight/collections/Main.java
@@ -3,49 +3,31 @@ package com.pluralsight.collections;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        final Map<Integer, Product> idToProduct = new HashMap<>();
+        final List<Product> products = new ArrayList<>();
+        products.add(ProductFixtures.door);
+        products.add(ProductFixtures.floorPanel);
+        products.add(ProductFixtures.window);
 
-        final Product defaultProduct = new Product(-1, "Whatever the customer wants", 100);
-
-        idToProduct.put(1, ProductFixtures.door);
-        idToProduct.put(2, ProductFixtures.floorPanel);
-        idToProduct.put(3, ProductFixtures.window);
-
-        idToProduct.forEach((key, value) -> System.out.println(key + " -> " + value));
-        System.out.println();
-
-        final Set<Map.Entry<Integer, Product>> entries = idToProduct.entrySet();
-        System.out.println(entries);
-        for (Map.Entry<Integer, Product> entry : entries) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-//            entry.setValue(ProductFixtures.door);
-        }
-        System.out.println();
-
-        final Set<Integer> ids = idToProduct.keySet();
-        System.out.println(ids);
-
-        ids.remove(1);
-        System.out.println(ids);
-        System.out.println(idToProduct);
-        System.out.println();
-
-        final Collection<Product> products = idToProduct.values();
         System.out.println(products);
         System.out.println();
 
-        products.remove(ProductFixtures.floorPanel);
+        Collections.rotate(products, 1);
         System.out.println(products);
-        System.out.println(idToProduct);
+        System.out.println();
+
+        Collections.rotate(products, 1);
+        System.out.println(products);
         System.out.println();
     }
 }
